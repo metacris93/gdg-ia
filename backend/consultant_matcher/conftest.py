57b57386a086ebaf4c6731cae218a3db2a8bee97
Andres Caceres <andres.caceres@thoughtworks.com>
2024-05-03 00:38:38 -0500

    Generated starter files for the django setup

---
+++ backend/consultant_matcher/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from consultant_matcher.users.models import User
+from consultant_matcher.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
