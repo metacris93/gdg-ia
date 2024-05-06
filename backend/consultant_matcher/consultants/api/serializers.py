da1ba279932f76a112aa07787cb3ea562c321a44
Andres Caceres <andres.caceres@thoughtworks.com>
2024-05-05 23:26:37 -0500

    Loading consultants from db

--- backend/consultant_matcher/consultants/api/serializers.py
+++ backend/consultant_matcher/consultants/api/serializers.py
@@ -1,13 +1,39 @@
 from rest_framework import serializers
 
-from consultant_matcher.consultants.models import Consultant
+from consultant_matcher.consultants.models import Consultant, TechStack, SoftSkill, Industry, AreaOfInterest
+
+class TechStackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TechStack
+        fields = ('id', 'name')
+
+class IndustrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Industry
+        fields = ('id', 'name')
+
+class SoftSkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SoftSkill
+        fields = ('id', 'name')
+
+class AreaOfInterestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AreaOfInterest
+        fields = ('id', 'name')
 
 
 class ConsultantSerializer(serializers.ModelSerializer[Consultant]):
     class Meta:
         model = Consultant
-        fields = ["name", "email", "description", "seniority"]
+        fields = '__all__'
 
-        extra_kwargs = {
-            "url": {"view_name": "api:consultant-detail", "lookup_field": "pk"},
-        }
+    tech_stacks = TechStackSerializer(many=True, read_only=True)
+    industries = IndustrySerializer(many=True, read_only=True)
+    soft_skills = SoftSkillSerializer(many=True, read_only=True)
+    areas_of_interest = AreaOfInterestSerializer(many=True, read_only=True)
+
+    extra_kwargs = {
+        "url": {"view_name": "api:consultant-detail", "lookup_field": "pk"},
+    }
+
